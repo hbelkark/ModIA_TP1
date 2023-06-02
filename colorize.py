b8ab551f21304d8e36736a89275ae1339abc43fc
Belkarkor Hamza <hamza.belkarkor@etu.inp-n7.fr>
2023-06-02 11:43:44 +0200

    add unet

--- colorize.py
+++ colorize.py
@@ -4,9 +4,10 @@ from tqdm import tqdm #used to generate progress bar during training
 
 import torch
 import torch.optim as optim 
+import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 from  torchvision.utils import make_grid #to generate image grids, will be used in tensorboard 
-
+import torchvision.utils
 from data_utils import get_colorized_dataset_loader # dataloarder
 from unet import UNet
 
@@ -14,49 +15,41 @@ from unet import UNet
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def train(net, optimizer, loader, epochs=5, writer=None):
-    criterion = ...
+    criterion = torch.nn.MSELoss()
     for epoch in range(epochs):
         running_loss = []
         t = tqdm(loader)
-        for x, y in t: # x: black and white image, y: colored image 
-            ...
-            ...
-            ...
-            ...
-            ...
-            ...
-            ...
-            ...
+        for x, y in t:
+            x, y = x.to(device), y.to(device)
+            outputs = net(x)
+            loss = criterion(outputs, y)
+            running_loss.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            t.set_description(f'training loss: {mean(running_loss)}')
         if writer is not None:
-            #Logging loss in tensorboard
             writer.add_scalar('training loss', mean(running_loss), epoch)
-            # Logging a sample of inputs in tensorboard
-            input_grid = make_grid(x[:16].detach().cpu())
-            writer.add_image('Input', input_grid, epoch)
-            # Logging a sample of predicted outputs in tensorboard
-            colorized_grid = make_grid(outputs[:16].detach().cpu())
-            writer.add_image('Predicted', colorized_grid, epoch)
-            # Logging a sample of ground truth in tensorboard
-            original_grid = make_grid(y[:16].detach().cpu())
-            writer.add_image('Ground truth', original_grid, epoch)
+            img_grid = torchvision.utils.make_grid(outputs[:16].detach().cpu())
+            writer.add_image('colorized', img_grid, epoch)
+            img_grid = torchvision.utils.make_grid(y[:16].detach().cpu())
+            writer.add_image('original', img_grid, epoch)
     return mean(running_loss)
-        
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str, default = 'Colorize', help='experiment name')
-    parser.add_argument('--data_path', ...)
-    parser.add_argument('--batch_size'...)
-    parser.add_argument('--epochs'...)
-    parser.add_argument('--lr'...)
+    parser.add_argument('--data_path', type=str, default = 'data/landscapes', help='dataset path')
+    parser.add_argument('--batch_size', type=int, default = int(32), help='batch_size')
+    parser.add_argument('--epochs', type=int, default = int(10), help='number of epochs')
+    parser.add_argument('--lr', type=float, default = float(1e-3), help='learning rate')
 
-    exp_name = ...
-    args = ...
-    data_path = ...
-    batch_size = ...
-    epochs = ...
-    lr = ...
+    args = parser.parse_args()
+    data_path = args.data_path
+    batch_size = args.batch_size
+    epochs = args.epochs
+    lr = args.lr
     unet = UNet().to(device)
     loader = get_colorized_dataset_loader(path=data_path, 
                                         batch_size=batch_size, 
@@ -65,9 +58,25 @@ if __name__=='__main__':
 
 
     optimizer = optim.Adam(unet.parameters(), lr=lr)
-    writer = SummaryWriter(f'runs/{exp_name}')
-    train(unet, optimizer, loader, epochs=epochs, writer=writer)
-    writer.add_graph(unet)
+    writer = SummaryWriter('runs/UNet')
+    loss = train(unet, optimizer, loader, epochs=epochs, writer=writer)
 
+    x, y = next(iter(loader))
+
+    with torch.no_grad():
+        all_embeddings = []
+        all_labels = []
+        for x, y in loader:
+            x , y = x.to(device), y.to(device)
+            embeddings = unet.get_features(x).view(-1, 128*28*28)
+            all_embeddings.append(embeddings)
+            all_labels.append(y)
+            if len(all_embeddings)>6:
+                break
+        embeddings = torch.cat(all_embeddings)
+        labels = torch.cat(all_labels)
+        writer.add_embedding(embeddings, label_img=labels, global_step=1)
+        writer.add_graph(unet, x.to(device))
+        
     # Save model weights
     torch.save(unet.state_dict(), 'unet.pth')
